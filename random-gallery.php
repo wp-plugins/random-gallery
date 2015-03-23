cefc1b2246de9baeb3ee59e379bdeab2d5476fa5
David Greenwold <David Greenwold@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-23 21:42:47 +0000

    First commit.
    git-svn-id: https://plugins.svn.wordpress.org/random-gallery/trunk@1119193 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ random-gallery.php
@@ -0,0 +1,14 @@
+<?php
+/** * Plugin Name: Random Gallery * Description: Random Gallery takes a list of photos (ids) and a number (shownum). It then picks that number of photos randomly from the list and and uses the new list to makes a standard gallery. Example: [random-gallery ids="1,2,3,5,8" shownum="3"] * Version: 00.01 * Author: DavidGreenwold * Author URI: http://www.greenwold.com/web-projects/#random-gallery * License: GPLv2 or later */
+/*	Copyright 2015 David Greenwold (email : david@greenwold.com)
+	This program is free software: you can redistribute it and/or modify	it under the terms of the GNU General Public License as published by	the Free Software Foundation version 2 or later. See <http://www.gnu.org/licenses/gpl-2.0.html>.
+	This program is distributed in the hope that it will be useful,	but WITHOUT ANY WARRANTY; without even the implied warranty of	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	GNU General Public License for more details.
+*/
+class dgplugins {
+function random_gallery($attr)  { // $attr is the default name given by wp to an array consisting of the key/value pairs defined by the user in the shortcode	// Sanitize the user input.
+	sanitize_text_field($attr['ids']);	sanitize_text_field($attr['shownum']);	$ids_in = explode(",", $attr['ids']); // Split the ids string into an array.	$count_in = count($ids_in); // Get the length of the array (the number of ids).	// Try to account for mistakes the user might make in entering shownum.
+	if( !isset($attr['shownum']) || empty($attr['shownum']) || $attr['shownum'] < 1 || $attr['shownum'] > $count_in ) {		$attr['shownum'] = $count_in;	}
+	// Create an array called $ids_out, which has a length equal to shownum and contains randomly selected values from $ids_in.	// Use a helper array called $rand, which also has a length equal to shownum. For each index, use the intermediate variable $randi. Only append it to $rand if it has not yet been used.		$rand[0] = mt_rand(0, $count_in - 1);	$ids_out[0] = $ids_in[$rand[0]];
+	for ($i = 1; $i < $attr['shownum']; $i++) {		$randi = mt_rand(0, $count_in - 1);		while (in_array($randi, $rand)) {			$randi = mt_rand(0, $count_in - 1);		}		$rand[$i] = $randi;		$ids_out[$i] = $ids_in[$rand[$i]];	}
+	// Use the new ids list ($ids_out) to call the regular gallery shortcode.	echo do_shortcode('[gallery ids='.implode(",", $ids_out).' orderby="rand"]');}}// Create a shortcode that calls the function.
+add_shortcode('random-gallery',array('dgplugins','random_gallery'));
---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Random Gallery ===
+Contributors: DavidGreenwold 
+Donate link: http://www.greenwold.com/web-projects/#random-gallery
+Tags: gallery, tiled, random, truncate, list, media, pictures, photos
+Requires at least: 3.0.1
+Tested up to: 4.1.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Random Gallery lets you display a random selection of photos from an arbitrarily large list.
+
+== Description ==
+
+Random Gallery takes a list of photos (ids) and a number (shownum). It then picks that number of photos randomly from the list and uses the new list to make a normal gallery.
+
+You can generate the list of ids by inserting a normal gallery shortcode, then copy/pasting the ids into the random-gallery shortcode, and finally deleting the normal gallery shortcode.
+
+Example: [random-gallery ids="1,2,3,5,8" shownum="3"]
+
+== Installation ==
+
+Drop random-gallery.php in the wp-content/plugins/ directory of your wordPress installation and activate it from Plugins page.
+
+== Frequently Asked Questions ==
+
+= What do I do if I have a question? =
+
+Send me an email: david.greenwold@gmail.com
+
+== Changelog ==
+
+= 00.01 =
+
+Added header and comments. Submitted to wp.
+
+= 00.00 =
+
+Just the code. Made for and deployed on client's website.
